0aa78489a2a5786d681ccccac9c8bd60bafcb563
atafs <atafs@iscte-iul.pt>
2016-06-09 16:36:09 +0100

    more

---
+++ proj005_nodeJS+mongDB/_bat/_2run-mongodb.bat
@@ -0,0 +1,5 @@
+REM run mongodb in a new terminal
+mongod --dbpath C:\Users\AdminPC\Documents\BackEnd_Javascript_nodeJS_1express\proj005_nodeJS+mongDB\nodeJS-plus-mongoDB\data
+
+REM in a different terminal (to open  a cmd to manipulate de mongoDB)
+mongo
